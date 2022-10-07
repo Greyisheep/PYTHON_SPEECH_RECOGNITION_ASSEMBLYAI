b519541f125c800669b3e4f8cb2f6352a2c363d7
Greyisheep <97015429+Greyisheep@users.noreply.github.com>
2022-10-07 16:08:50 +0100

    AssemblyAI

---
+++ Sentiment_Analysis/api.py
@@ -0,0 +1,84 @@
+import requests
+from api_secrets import API_KEY_ASSEMBLYAI
+import time
+import json
+
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
+
+headers_auth_only = {"authorization": API_KEY_ASSEMBLYAI}
+
+
+headers = {
+    'authorization': API_KEY_ASSEMBLYAI,
+    'content-type': "application/json"
+    }
+
+
+CHUNK_SIZE = 5_242_880
+
+def upload(filename):
+    def read_file(filename):
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(CHUNK_SIZE)
+                if not data:
+                    break
+                yield data
+                
+    upload_response = requests.post(upload_endpoint,
+                            headers=headers_auth_only,
+                            data=read_file(filename))
+
+
+    audio_url = upload_response.json()["upload_url"]
+
+    return audio_url
+
+
+def transcribe(audio_url, sentiment_analysis):
+    transcript_request = {
+        "audio_url": audio_url,
+        "sentiment_analysis": sentiment_analysis
+        }
+    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+
+    job_id = transcript_response.json()['id']
+    return job_id
+
+
+def poll(transcript_id):
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    polling_response = requests.get(polling_endpoint, headers=headers)
+    return polling_response.json()
+
+
+def get_transcription_result_url(audio_url, sentiment_analysis):
+    transcribe_id = transcribe(audio_url, sentiment_analysis)
+    while True:
+        data = poll(transcribe_id)
+        if data['status'] == 'completed':
+            return  data, None
+        elif data['status'] == 'error':
+            return data, data["error"]
+
+        print('Waiting 30 seconds...')
+        time.sleep(30)     
+
+
+def save_transcript(audio_url, fiename, sentiment_analysis=False):
+    data, error = get_transcription_result_url(audio_url, sentiment_analysis)
+
+    if data:
+        text_filename = fiename + '.txt'
+        with open(text_filename, "w") as f:
+            f.write(data['text'])
+
+        if sentiment_analysis:
+            text_filename = fiename  + '_sentiments.json'
+            with open(text_filename, "w") as f:
+                sentiments = data["sentiment_analysis_results"]
+                json.dump(sentiments, f, indent=4)
+        print('Transcription saved!!!')
+    elif error:
+        print("Error!!!", error)
